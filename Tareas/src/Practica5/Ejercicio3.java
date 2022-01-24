02e6ff9dcb5fc4a0413c52a20a2daa489bac89db
tay-rel <sirenita0197@gmail.com>
2022-01-24 14:43:30 +0100

    Usar indexOf

--- Tareas/src/Practica5/Ejercicio3.java
+++ Tareas/src/Practica5/Ejercicio3.java
@@ -5,11 +5,9 @@ public class Ejercicio3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String cadena ="“Estamos viviendo en un submarino amarillo.\n No tenemos nada que hacer."
-				+ " En el interior del submarino se está muy apretado.\n Así que estamos leyendo todo el día."
-				+ " Vamos a salir en 5 días\".";
+		String cadena ="“Estamos viviendo en un submarino amarillo.\n No tenemos nada que hacer. En el interior del submarino se está muy apretado.\n Así que estamos leyendo todo el día. Vamos a salir en 5 días\".";
 		
-		String pedirPalabra="en";
+		String pedirPalabra=" en ";
 		
 		System.out.println(cadena);
 		System.out.println("\nLa palabra "+" '" + pedirPalabra+" '"+" se repite: "+obtenerNumeroVecesSubCadena(cadena, pedirPalabra));
@@ -20,17 +18,18 @@ public class Ejercicio3 {
 	public static int obtenerNumeroVecesSubCadena(String cadena ,String subcadena) {
 		
 		int contador=0;
-		int veces=0;
-		int busca =cadena.indexOf(subcadena, veces);
-		StringBuilder contenido= new StringBuilder(cadena);
+		String aux=""; //forma las pequeñas subcadenas para comparar
+		int limiteSb= subcadena.length();
 		
-		for (int i = 0; i < cadena.length(); i++) {
-			if() {
-				contador ++;
+		for (int i = 0; i <= cadena.length() -limiteSb ; i++) { //recorro cadena - limiteSb
+			aux=cadena.substring(i, i+limiteSb);
+			
+			if(subcadena.equals(aux)) {
+				contador++;
 			}
-				
+			aux=" ";//Para evaluar de nuevo la cadena el aux debe estar limpio
+			
 		}
-
 		return contador;
 		
 	}
