511f18421b1341c5f95814515308a7f404109332
tay-rel <sirenita0197@gmail.com>
2022-03-17 19:45:17 +0100

    corregido

---
+++ Tareas/src/Practica7/Ejercicio01.java
@@ -0,0 +1,23 @@
+package Practica7;
+
+public class Ejercicio01 {
+	public static void main(String[] args) {
+		Punto coordenada=new Punto();//Instanciamos la clase
+		
+		coordenada.x=4.5;
+		coordenada.y=8.6;
+		
+		System.out.println("Coordenada: "+coordenada.x+" "+coordenada.y);
+		//Modificar alguna de sus coordenadas accediendo directamente al atributo correspondiente
+		coordenada.y=3.5;
+		System.out.println("Coordenada actual es: "+coordenada.x+" "+coordenada.y);
+		
+	}	
+}
+class Punto{
+	//Propiedades
+	public double x;
+	public double y;
+	
+	
+}
---
+++ Tareas/src/Practica7/Ejercicio02.java
@@ -0,0 +1,9 @@
+package Practica7;
+
+public class Ejercicio02 {
+	public static void main(String[] args) {
+		
+	}
+
+}
+
