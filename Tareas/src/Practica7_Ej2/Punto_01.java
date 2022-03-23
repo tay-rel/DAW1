ef162ca744d50013095417df8100338e86bbef5f
tay-rel <sirenita0197@gmail.com>
2022-03-23 12:04:48 +0100

    Actualizado

--- Tareas/src/Practica7_Ej2/Punto_01.java
+++ Tareas/src/Practica7_Ej2/Punto_01.java
@@ -5,27 +5,52 @@ package Practica7_Ej2;
 
 public class Punto_01 {
 	public static void main(String[] args) {
+
+		Punto p = new Punto(0, -5);
 		
 		
 		
-		Punto p=new Punto(0,4);
+
+		System.out.println("Su coordenada ( " + p.getX() + ", " + p.getY() + " ) ");
 		
-		System.out.println("Su coordenada ( " + p.getX()+", "+ p.getY()+" ) ");
-		System.out.println("Esta ubicada "+p.calcularCuadrante() +" en el cuadrante");
+		if(p.getX()== 0 && p.getY()== 0) {
+			System.out.println("Esta en el origen");
+		}
+		if (p.getX()==0) {
+			System.out.println("Esta en el eje X ");
+		}
+		if (p.getY()==0) {
+			System.out.println("Esta en el eje Y");
+		}
+		
+		if(p.getX()> 0 && p.getY()>0) {
+			System.out.println("Esta en el cuadrante I");
+		}
+		
+		else if (p.getX() <0 && p.getY()>0) {
+			System.out.println("Esta en el cuadrante II");
+		}
+		else if (p.getX()<0 && p.getY()<0) {
+			System.out.println("Esta en el cuadrante III");
+		}
+		else if (p.getX()>0 && p.getY()<0) {
+			System.out.println("Esta en el cuadrante VI");
+		}
+	
+			
 	}
-  
+
 }
-	  
 
-class Punto{
-		private double x;
-		private double y;
+class Punto {
+	private double x;
+	private double y;
 
-	  // Constructor
-	  public Punto(double x, double y) {
-	    this.x = x;
-	    this.y = y;
-	  }
+	// Constructor
+	public Punto(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
 
 	public double getX() {
 		return x;
@@ -35,26 +60,27 @@ class Punto{
 		return y;
 	}
 
-
-	  
 	// devuelve el cuadrante
-	  public int calcularCuadrante() {
-	    if (x > 0.0 && y > 0.0) {
-	      return 1;
-	    } else if (x < 0.0 && y > 0.0) {
-	      return 2;
-	    } else if (x < 0.0 && y < 0.0) {
-	      return 3;
-	    } else if (x > 0.0 && y < 0.0) {
-	      return 4;
-	    } else {
-	      // (x==0.0 || y==0.0)
-	      return 0;
-	    }
-	  }
-	  
-	  //valoidar si esta en el eje de coordenadas
+	/*public int calcularCuadrante() {
+		
+		if (x > 0.0 && y > 0.0) {
+			return 1;
+		} else if (x < 0.0 && y > 0.0) {
+			return 2;
+		} else if (x < 0.0 && y < 0.0) {
+			return 3;
+		} else if (x > 0.0 && y < 0.0) {
+			return 4;
+		} else if(x == 0.0 || y== 0.0){
+			return 0;
+		}else {
+			return -1;
+		}
+		
+		
+		
+	}*/
+
+	// validar si esta en el eje de coordenadas
 
 }
-
-	  
